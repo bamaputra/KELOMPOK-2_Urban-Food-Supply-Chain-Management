aee57ed4af583c5dd76e8713c907983dfe7a2ee3
Deco Ardiandra <decoardiandra.2025@student.uny.ac.id>
2026-05-14 23:34:55 +0700

    Fix indentation and syntax errors in modul_4.py

--- tsp-asd-kelompok-02/src/modules/modul_4.py
+++ tsp-asd-kelompok-02/src/modules/modul_4.py
@@ -2,22 +2,16 @@
 from models import product 
 class bst node 
 
-if (  def init unit  self. rooy
+if (  def init unit  self. root
 
+def _search_rec(self, node, kode):
+        if node is None or node.produk.kode == kode:
+            return node.produk if node else None
+        if kode < node.produk.kode:
+            return self._search_rec(node.left, kode)
+        return self._search_rec(node.right, kode)
+ 
+    def update_stok(self, kode, delta):
+        produk = self.search(kode)
+        if produk:
 
-hhc 05 define
-
-k
-k
-k
-k
-k
-k
-k
-k
-k
-k
-k
-k
-k
-k
