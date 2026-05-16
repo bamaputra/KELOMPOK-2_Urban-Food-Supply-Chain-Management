bc121275dedd56f06d34fd4b09c020059286f2c8
muhammad4907ft2025-bit <muhammad4907ft.2025@student.uny.ac.id>
2026-05-16 22:04:12 +0700

    Refactor graph structure and add product models

--- tsp-asd-kelompok-02/src/modules/modul_1.py
+++ tsp-asd-kelompok-02/src/modules/modul_1.py
@@ -1,73 +1,33 @@
-class Edge:
-    def _init_(self, dest, jarak_km, biaya_per_km):
-        self.dest = dest
-        self.jarak_km = jarak_km
-        self.biaya_per_km = biaya_per_km
+# models.py
+from dataclasses import dataclass
+ 
+KATEGORI_PRODUK = ['SAYUR', 'BUAH', 'DAGING', 'IKAN', 'BAHAN_POKOK']
+TIPE_NODE = ['PETANI', 'DISTRIBUTOR', 'PASAR', 'GUDANG']
+ 
+ 
+@dataclass
+class Produk:
+    kode: str
+    nama: str
+    kategori: str
+    harga_satuan: float
+    stok: int
+    masa_kadaluarsa_hari: int
+ 
+ 
+@dataclass
+class Pengiriman:
+    pengiriman_id: int
+    dari_node: str
+    ke_node: str
+    kode_produk: str
+    jumlah: int
+    prioritas: int
+    waktu_kirim: float
+ 
+ 
+class LLNode:
+    """Node untuk linked list"""
+    def __init__(self, data=None):
+        self.data = data
         self.next = None
-
-class GraphRantaiPasok:
-    def _init_(self):
-        self.adj = {}
-        self.tipe_node = {}
-
-    def tambah_node(self, node_id, tipe):
-        """Menambah node ke graph - Big-O: O(1)"""
-        if node_id not in self.adj:
-            self.adj[node_id] = None
-            self.tipe_node[node_id] = tipe
-
-    def tambah_jalur(self, u, v, jarak, biaya_km):
-        """Menambah jalur edge antara dua node"""
-        new_edge = Edge(v, jarak, biaya_km)
-        new_edge.next = self.adj[u]
-        self.adj[u] = new_edge
-
-        new_edge = Edge(u, jarak, biaya_km)
-        new_edge.next = self.adj[v]
-        self.adj[v] = new_edge
-
-  def tetangga(self, u):
-        """Mendapatkan semua tetangga dari node u"""
-        neighbors = []
-        current = self.adj.get(u, None)
-        while current:
-            neighbors.append((current.dest, current.jarak_km, current.biaya_per_km))
-            current = current.next
-        return neighbors
-
-    def bfs_audit(self, start):
-        """BFS untuk audit konektivitas - Big-O: O(V+E)"""
-        visited = set()
-        from collections import deque
-        queue = deque([start])
-        visited.add(start)
-        
-        while queue:
-            node = queue.popleft()
-            current = self.adj.get(node, None)
-            while current:
-                if current.dest not in visited:
-                    visited.add(current.dest)
-                    queue.append(current.dest)
-                current = current.next
-        return visited
-
-    def dfs_audit(self, start):
-        """DFS untuk audit konektivitas - Big-O: O(V+E)"""
-        visited = set()
-        stack = [start]
-        
-        while stack:
-            node = stack.pop()
-            if node not in visited:
-                visited.add(node)
-                current = self.adj.get(node, None)
-                while current:
-                    if current.dest not in visited:
-                        stack.append(current.dest)
-                    current = current.next
-        return visited
-
-    def get_all_nodes(self):
-        return list(self.adj.keys())
-
