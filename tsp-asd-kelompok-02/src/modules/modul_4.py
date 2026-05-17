2e709d67a5d65b00c88ea29186ef01ffdb5a7e4f
Deco Ardiandra <decoardiandra.2025@student.uny.ac.id>
2026-05-17 10:20:14 +0700

    Implement PriorityQueueKirim class for shipments
    Added a priority queue implementation for handling shipments.

--- tsp-asd-kelompok-02/src/modules/modul_4.py
+++ tsp-asd-kelompok-02/src/modules/modul_4.py
@@ -163,7 +163,39 @@ def main():
     buffer_gudang = {}
     kirim_counter = 0
  
-   
+   class PriorityQueueKirim:
+    def __init__(self):
+        self.head = None
+        self._size = 0
+ 
+    def enqueue(self, pengiriman):
+        new_node = LLNode(pengiriman)
+        if self.head is None or pengiriman.prioritas < self.head.data.prioritas:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while (current.next is not None
+                   and current.next.data.prioritas <= pengiriman.prioritas):
+                current = current.next
+            new_node.next = current.next
+            current.next = new_node
+        self._size += 1
+ 
+    def dequeue(self):
+        if self.head is None:
+            return None
+        pengiriman = self.head.data
+        self.head = self.head.next
+        self._size -= 1
+        return pengiriman
+ 
+    def __len__(self):
+        return self._size
+ 
+    def is_empty(self):
+        return self.head is None
+
 
 
 
