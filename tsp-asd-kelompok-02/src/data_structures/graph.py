5687bf57846c7a01ee1034bb09cee8b2177f1e09
Ahmad Niro Yogara <ahmadniro.2025@student.uny.ac.id>
2026-05-19 22:09:55 +0700

    Update graph.py

--- tsp-asd-kelompok-02/src/data_structures/graph.py
+++ tsp-asd-kelompok-02/src/data_structures/graph.py
@@ -1,28 +1,77 @@
-from test_linked_list import LLNode
- 
-class Stack:
+class Edge:
+    def __init__(self, dest, jarak_km, biaya_per_km):
+        self.dest = dest
+        self.jarak_km = jarak_km
+        self.biaya_per_km = biaya_per_km
+        self.next = None
+ 
+ 
+class GraphRantaiPasok:
     def __init__(self):
-        self.top = None
-        self._size = 0
- 
-    def push(self, data):
-        new_node = LLNode(data)
-        new_node.next = self.top
-        self.top = new_node
-        self._size += 1
- 
-    def pop(self):
-        if self.top is None:
-            return None
-        data = self.top.data
-        self.top = self.top.next
-        self._size -= 1
-        return data
- 
-    def __len__(self):
-        return self._size
- 
-    def is_empty(self):
-        return self.top is None
- 
-
+        self.adj = {}
+        self.tipe_node = {}
+ 
+    def tambah_node(self, node_id, tipe):
+        if node_id not in self.adj:
+            self.adj[node_id] = None
+            self.tipe_node[node_id] = tipe
+ 
+    def tambah_jalur(self, u, v, jarak, biaya_km):
+        new_edge = Edge(v, jarak, biaya_km)
+        new_edge.next = self.adj[u]
+        self.adj[u] = new_edge
+ 
+        new_edge = Edge(u, jarak, biaya_km)
+        new_edge.next = self.adj[v]
+        self.adj[v] = new_edge
+ 
+    def tetangga(self, u):
+        neighbors = []
+        current = self.adj.get(u, None)
+        while current:
+            neighbors.append((current.dest, current.jarak_km, current.biaya_per_km))
+            current = current.next
+        return neighbors
+ 
+ 
+def dijkstra_biaya(graph, asal):
+    INF = float('inf')
+    dist = {v: INF for v in graph.adj}
+    parent = {v: None for v in graph.adj}
+    dist[asal] = 0.0
+    visited = set()
+ 
+    while len(visited) < len(graph.adj):
+        min_dist = INF
+        u = None
+        for v in graph.adj:
+            if v not in visited and dist[v] < min_dist:
+                min_dist = dist[v]
+                u = v
+ 
+        if u is None:
+            break
+ 
+        visited.add(u)
+ 
+        current = graph.adj[u]
+        while current:
+            v = current.dest
+            if v not in visited:
+                weight = current.jarak_km * current.biaya_per_km
+                if dist[u] + weight < dist[v]:
+                    dist[v] = dist[u] + weight
+                    parent[v] = u
+            current = current.next
+ 
+    return dist, parent
+ 
+ 
+def get_path(parent, target):
+    path = []
+    current = target
+    while current is not None:
+        path.append(current)
+        current = parent.get(current, None)
+    return list(reversed(path))
+ 
