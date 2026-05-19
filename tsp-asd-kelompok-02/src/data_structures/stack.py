3a449a7e5240ef29d469468d6390780a4e90d466
muhammad4907ft2025-bit <muhammad4907ft.2025@student.uny.ac.id>
2026-05-19 19:14:49 +0700

    Add Stack class with basic operations
    Implemented a Stack class with push, pop, and size methods.

--- tsp-asd-kelompok-02/src/data_structures/stack.py
+++ tsp-asd-kelompok-02/src/data_structures/stack.py
@@ -1,1 +1,28 @@
+from test_linked_list import LLNode
+ 
+class Stack:
+    def __init__(self):
+        self.top = None
+        self._size = 0
+ 
+    def push(self, data):
+        new_node = LLNode(data)
+        new_node.next = self.top
+        self.top = new_node
+        self._size += 1
+ 
+    def pop(self):
+        if self.top is None:
+            return None
+        data = self.top.data
+        self.top = self.top.next
+        self._size -= 1
+        return data
+ 
+    def __len__(self):
+        return self._size
+ 
+    def is_empty(self):
+        return self.top is None
+ 
 
